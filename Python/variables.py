1e8d5206b149fb97136026058ca8a5eaec2d63b8
Nikhil <nikhil20sy@gmail.com>
2026-01-14 23:49:22 +0530

    modified comments

--- Python/variables.py
+++ Python/variables.py
@@ -4,6 +4,6 @@ language = "Python"
 #We can declare string in both single and double quotes
 '''
 But when we declare variables in single quotes it is completely string 
-and no values can be decryptedgit add .
+and no values can be decrypted
 '''
 print(f'We are learning {topic} in {language}')
