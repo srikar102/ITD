ede0bfc1be006f9b1db17d4357d2225879fefb76
Nikhil <nikhil20sy@gmail.com>
2026-01-14 23:46:55 +0530

    variable file is added

---
+++ Python/variables.py
@@ -0,0 +1,9 @@
+topic = 'variables'
+language = "Python"
+
+#We can declare string in both single and double quotes
+'''
+But when we declare variables in single quotes it is completely string 
+and no values can be decryptedgit add .
+'''
+print(f'We are learning {topic} in {language}')
